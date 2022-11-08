8470fc6f67e72f967164b91f979931c36d5e51f8
Zebin Zhou <zb.zhou0922@gmail.com>
2022-11-07 16:03:43 -0800

    Updates

---
+++ precache-manifest.ef3457d6f0bd2e8b759ddabe0775ec03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "050a1a2ef1c8a378b5a4",
+    "url": "/robotfriends/static/css/main.4be4050b.chunk.css"
+  },
+  {
+    "revision": "050a1a2ef1c8a378b5a4",
+    "url": "/robotfriends/static/js/main.050a1a2e.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robotfriends/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "ef74953189533353496c",
+    "url": "/robotfriends/static/css/2.32ed38e5.chunk.css"
+  },
+  {
+    "revision": "ef74953189533353496c",
+    "url": "/robotfriends/static/js/2.ef749531.chunk.js"
+  },
+  {
+    "revision": "68476f975b545b92a205",
+    "url": "/robotfriends/static/js/runtime~main.68476f97.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3ae2d12a69b964c4d990cef310570f46",
+    "url": "/robotfriends/index.html"
+  }
+];
